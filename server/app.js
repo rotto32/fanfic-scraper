65b51ee0996848d104557fe4da0a4ae4e9a0e344
Rachael Otto <rachael.o@givingcompass.org>
2020-09-13 16:47:16 -0700

    fix issue with chapters being added out of order

--- server/app.js
+++ server/app.js
@@ -8,6 +8,28 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
+const axiosRecursive = function (baseURL, ficName, currentChapter, maxChapter) {
+  if (currentChapter <= maxChapter) {
+    axios.get(baseURL+'/'+currentChapter)
+    .catch((err) => {
+      console.log(err);
+    })
+    .then((res) => {
+      let chapterText = $('#storytext', res.data).html();
+      fs.appendFile(path.join(__dirname + '/public/fics/'+ficName+'.txt'), chapterText, (err) => {
+        if (err) {
+          console.log('Error appending file', err);
+          return;
+        } else {
+          axiosRecursive(baseURL, ficName, currentChapter+1, maxChapter);
+        }
+      });
+    });
+  } else {
+    return;
+  }
+}
+
 app.use(cors());
 
 app.use(bodyParser.json());
@@ -39,21 +61,7 @@ app.post('/submit',  (req, res) => {
 
       fs.stat(path.join(__dirname + '/public/fics/'+ficName+'.txt'), function(err, stat) {
         if (err === null) {
-          while (chapter <= maxChapter) {
-            chapter++;
-            axios.get(baseURL+'/'+chapter)
-              .catch((err) => {
-                console.log(err);
-              })
-              .then((res) => {
-                const chapterText = $('#storytext', res.data).html();
-                  fs.appendFile(path.join(__dirname + '/public/fics/'+ficName+'.txt'), chapterText, (err) => {
-                    if (err) {
-                      console.log('Error appending file', err);
-                    }
-                  });
-              })
-          }
+          axiosRecursive(baseURL, ficName, chapter+1, maxChapter);
         }
       });
 
