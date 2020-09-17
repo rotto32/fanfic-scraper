08ecbb554130ec1456a0c2ea49a50e07c3cf4ff1
Rachael Otto <rachael.o@givingcompass.org>
2020-09-17 11:01:50 -0600

    update text and link to better utilize new link functionality

--- server/public/resources/scripts/index.js
+++ server/public/resources/scripts/index.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         const timeMinutes = Math.floor(((Number(maxChap)-Number(minChap) + 1) * 3)/60);
         const ficLink = '/fics/' + ficName + '.html';
         $('#info-submit').hide();
-        $('#fic-link').append('<p>After '+ timeMinutes + ' minute(s), go to <a href="' + ficLink + '">'+ficName+'</a>.<br /> If the entire fic is not there, wait an additional couple of minutes and reload the page.</p>');
+        $('#fic-link').append('<p>After '+ timeMinutes + ' minute(s), go to <a href="' + ficLink + '" target="_blank">'+ficName+'</a>.<br /> If the entire fic is not there, wait an additional couple of minutes and reload the page.</p>');
         axios.post('/submit', {
             'url': url,
             'maxChap': maxChap,
